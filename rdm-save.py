416438efade0537529591fa521b0af6a3ccb0ec6
Taran <74196907+Taranks7@users.noreply.github.com>
2020-12-17 16:13:08 +0000

    Update rdm-save.py

--- rdm-save.py
+++ rdm-save.py
@@ -1,24 +1,61 @@
 import os
-count=0 # count default
-f_path = './home/taran/projects/AVIMA' # set path
-#loop 
-while os.path.isdir(f_path):
-    for file in os.listdir(f_path): # loop through the folder
-        print(file)   # print text to keep track the process
-        if file.endswith('.json'):
-            count+=1
-            print('+1')
+from pathlib import Path
+
+#starting path 
+fpath = './home/taran/projects/AVIMA' 
+p = Path(fpath)
+count=1 # count default
+
+#create loop through all files in AVIMA folder
+
+for file in p.glob('**/*'):
+    if file.endswith('.json'):
+        execfile('calc-dissim.py')
+        plt.savefig('img'+str(count)+'.png')
+        count +=1
 
 
-fpath = '/home/taran/projects/AVIMA/1/Meadows_avima-image-version1_v_v2_better-hound_2_tree.json'
+
+#while os.path.isdir(fpath):
+    #for file in os.listdir(fpath): # loop through the folder
+        #print(file)   # print text to keep track the process
+        #if file.endswith('.json'):
+            #count+=1
+            #print(file+'+1')
+
+#from subprocess import call 
+
+#class CallPy(file):
+    
+    #def __init__(self, path = fpath)
+       #self.path = path 
+    
+    #def call_python_file(self):
+        
+        
+        
+        
+        #call(["Python3", "{}".format(self.path)])
 
 
-#plt.plot(euc_dist)
-img = plt.imshow(euc_dist)
-#save figures 
-img_num = 1
-for _ in range(len(participants)):
-    plt.savefig('img'+str(img_num)+'.png')
-    img_num += 1
+#for each each .json file, run calc-dissim.py 
+#each rdm is img = imshow(euc_dist)
 
-plt.show()
+
+
+#File = namedtuple('File')
+
+#empty list 
+#files = []
+
+
+
+
+
+            #call calc-dissim.py 
+            #subprocess.run(['open', calc-dissim], check=True)
+
+
+#fpath = '/home/taran/projects/AVIMA/1/Meadows_avima-image-version1_v_v2_better-hound_2_tree.json'
+
+
