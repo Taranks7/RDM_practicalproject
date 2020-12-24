f9e89b4e6be11e971d013a069add53aa1187f606
Taranjit Sehmbi <74196907+Taranks7@users.noreply.github.com>
2020-12-24 20:55:04 +0000

    label x and y axes

--- plot-rdm.py
+++ plot-rdm.py
@@ -14,9 +14,15 @@ with open(fpath) as fhandle:
    
 #inspect rdm stimuli labels 
 data['stimuli']
-#create array containing all labels 
-rdm_names = np.array(data['stimuli'])
 
+#contain all labels for y axis and x axis seperately  
+y_names = []
+for i in stim:
+   y_names = (i['name'])
+   
+x_names = []
+for i in stim:
+   x_names = (i['name'])
 
 #inspect rdm data   
 data['rdm']
@@ -31,6 +37,25 @@ srdm = squareform(rdm_array)
 plt.plot(srdm)
 plt.imshow(srdm)
 plt.colorbar(mappable = None, cax = None, ax = None)
+
+#label x and y axis on rdm 
+
+fig, ax = plt.subplots()
+rdm = ax.imshow(srdm)
+
+ax.set_xticks(np.arange(len(x_names)))
+ax.set_yticks(np.arange(len(y_names)))
+
+ax.set_xticklabels(x_names)
+ax.set_yticklabels(y_names)
+
+plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+         rotation_mode="anchor")
+
+for i in range(len(x_names)):
+    for j in range(len(y_names)):
+        text = ax.text(j, i, srdm[i, j])
+
 plt.show()
 
 
