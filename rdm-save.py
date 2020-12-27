971ba00276873b7ea9c74339bc0cbfc31aa3f2e1
Taranjit Sehmbi <74196907+Taranks7@users.noreply.github.com>
2020-12-27 16:19:44 +0000

    Update rdm-save.py

--- rdm-save.py
+++ rdm-save.py
@@ -1,7 +1,5 @@
 import os
-from pathlib import Path
-import matplotlib 
-import matplotlib.pyplot as plt 
+#from pathlib import Path
 
 # define fpath 
 count=0 # count default
@@ -14,11 +12,12 @@ for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         if file.startswith('Meadows'):
             filepath.append(os.path.join(subdir, file))
+            fpath = filepath[count]
+            exec(open('plot-rdm.py').read())
+            count +=1
             
-p = Path(path)
-for file in p.glob('**/*'):
-    if file.endswith('.json'):
-        fpath= filepath[count]
-        exec(open('plot-rdm.py').read())
-        #plt.savefig('img'+str(count)+'.png')
-        count +=1
+#p = Path(path)
+#for file in p.glob('**/*'):
+    #if file.startswith('Meadows'):
+        #fpath= filepath[count]
+        
