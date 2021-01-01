e91d80d2abffed547e740fd4ea8df70932f68544
Taranjit Sehmbi <74196907+Taranks7@users.noreply.github.com>
2021-01-01 15:34:16 +0000

    Add files via upload

--- AVIMA/plotrdm.py
+++ AVIMA/plotrdm.py
@@ -1,48 +1,45 @@
-with open(fpath) as fhandle:
-    data = json.load(fhandle)
- #inspect rdm stimuli labels 
-stim = data['stimuli']
+def plot_rdm(fpath):
+    import numpy as np
+    import matplotlib
+    import matplotlib.pyplot
+    import matplotlib.pyplot as plt
+    import scipy
+    import scipy.spatial
+    import scipy.spatial.distance as sd
+    from scipy.spatial.distance import squareform
+    import json 
+    
+    with open(fpath) as fhandle:
+        data = json.load(fhandle)
+	 #inspect rdm stimuli labels 
+    stim = data['stimuli']
 
-type(stim)
-type(stim[0])
-len(stim)
-#stim is a list of 50 dict elements 
+	#contain all labels for y axis and x axis seperately  
+    y_names = []
+    for i in stim:
+        y_names.append(i['name'])
 
-#contain all labels for y axis and x axis seperately  
-y_names = []
-for i in stim:
-    y_names.append(i['name'])
+    x_names = []
+    for i in stim:
+        x_names.append(i['name'])
 
-x_names = []
-for i in stim:
-    x_names.append(i['name'])
+	#create rdm array and squareform 
+    rdm_array = np.array(data['rdm'])
+    srdm = squareform(rdm_array)
 
-#inspect rdm data   
-data['rdm']
-rdm_array = np.array(data['rdm'])
-rdm_array.shape
+	#label x and y axis on rdm 
+    fig, ax = plt.subplots()
+    rdm = ax.imshow(srdm)
 
-#plt.plot(rdm_array.T)
-#plt.show()
+    ax.set_xticks(np.arange(len(x_names)))
+    ax.set_yticks(np.arange(len(y_names)))
 
-srdm = squareform(rdm_array)
-
-#label x and y axis on rdm 
-fig, ax = plt.subplots()
-rdm = ax.imshow(srdm)
-
-ax.set_xticks(np.arange(len(x_names)))
-ax.set_yticks(np.arange(len(y_names)))
-
-ax.set_xticklabels(x_names)
-ax.set_yticklabels(y_names)
-
-plt.setp(ax.get_xticklabels(), rotation=90, ha="right", rotation_mode="anchor")
-plt.plot(srdm)
-plt.imshow(srdm)
-plt.colorbar(mappable = None, cax = None, ax = None)
-fig.subplots_adjust(bottom=0.23)
-#plt.show()
-
-#save plot
-plt.savefig('rdm'+'.png')
+    ax.set_xticklabels(x_names)
+    ax.set_yticklabels(y_names)
+    plt.setp(ax.get_xticklabels(), rotation=90, ha="right", rotation_mode="anchor")
+    plt.plot(srdm)
+    plt.imshow(srdm)
+    plt.colorbar(mappable = None, cax = None, ax = None)
+    fig.subplots_adjust(bottom=0.23)
+import matplotlib.pyplot as plt
+plt.savefig('rdm.png')
--- AVIMA/rdmsave.py
+++ AVIMA/rdmsave.py
@@ -1,20 +1,19 @@
 #import libraries
 import numpy as np
-import matplotlib 
-import matplotlib.pyplot as plt 
+import matplotlib
+import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import scipy
 import scipy.spatial
 import scipy.spatial.distance as sd
 from scipy.spatial.distance import squareform
 import os
 import json 
-from subprocess import * 
-import time
 
 # define fpath 
 #i.e. fpath[0] will be the first filepath... 
 
-path = '/home/taran/projects/AVIMA' 
+path = '/home/taran/RDM_researchproject/AVIMA' 
 rootdir = path
 filepath = []
 for subdir, dirs, files in os.walk(rootdir):
@@ -23,6 +22,7 @@ for subdir, dirs, files in os.walk(rootdir):
             count=0 # count default
             filepath.append(os.path.join(subdir, file))
             fpath = filepath[count]
-            Popen('plotrdm.py')
-            time.sleep(1)
+            f = open('plotrdm.py', 'r')
+            from plotrdm import plot_rdm
+            plot_rdm(fpath)
             count +=1      
