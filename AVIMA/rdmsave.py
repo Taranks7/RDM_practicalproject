dba6a73071e8a04ff9ae2e0b03f27546505c694b
Taranjit Sehmbi <74196907+Taranks7@users.noreply.github.com>
2021-01-01 23:59:36 +0000

    include count in loop

--- AVIMA/rdmsave.py
+++ AVIMA/rdmsave.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     count = 0
     for count, path in enumerate(glob.glob(f'{basedir}/**/Meadow*.json', recursive=True)):
         print(f"processing {path}")
+        count +=1
         output_image = f'rdm_{count:01}.pdf'
         print(f"output image will be {output_image}")
         plot_rdm(path, output_image)
-    print(f"processed {count+1} files")
+    print(f"processed {count} files")
