cd9c3046415e6420cadab6716440d204057b3d11
Taranjit Sehmbi <74196907+Taranks7@users.noreply.github.com>
2021-01-09 23:08:28 +0000

    moved count to end of the loop

--- AVIMA/disrdmsave.py
+++ AVIMA/disrdmsave.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     count = 0
     for count, path in enumerate(sorted(glob.glob(f'{basedir}/**/Meadow*.json', recursive=True))):
         print(f"processing {path}")
-        count +=1
         output_image = f'dis_rdm_{count:02}.pdf'
         print(f"output image will be {output_image}")
         calc_dissim(path, output_image)
+        count +=1
     print(f"processed {count} files")
